80e4e320057d995fb87ef9c920cd58b698fc6b58
faruko <faruko@duzey.com.tr>
2025-08-18 22:24:09 +0300

    .

--- biterbot/main.py
+++ biterbot/main.py
@@ -12,26 +12,26 @@ async def main():
     client = BinancePublicClient()
 
     # Feed'i başlat
-    feed = OhlcvFeed(client, bus, limit=200, buffer_seconds=2)
+    feed = OhlcvFeed(client, bus, limit=200, buffer_seconds=3)
     feed.start_many(
         {
-            "BTCUSDT": ["15m", "1h", "4h", "1d"],
-            "ETHUSDT": ["15m", "1h", "4h", "1d"],
-            "SOLUSDT": ["15m", "1h", "4h", "1d"],
-            "XRPUSDT": ["15m", "1h", "4h", "1d"]
+            "BTCUSDT": ["15m", "1h", "4h"],
+            "ETHUSDT": ["15m", "1h", "4h"],
+            "SOLUSDT": ["15m", "1h", "4h"],
+            "XRPUSDT": ["15m", "1h", "4h"]
         }
     )
 
     # Sinyaller
     tsa = SignalAdaptor(
         bus,
-        TrendSignalGen("Trend_ETHUSDT_15m", "ETHUSDT", "15m", hysteresis_th=0.002),
+        TrendSignalGen("Trend_ETHUSDT_15m", "ETHUSDT", "15m", hysteresis_th=0.002, confirm_bars=5),
         TrendSignalGen("Trend_ETHUSDT_1h",  "ETHUSDT", "1h",  hysteresis_th=0.005),
         TrendSignalGen("Trend_ETHUSDT_4h",  "ETHUSDT", "4h",  hysteresis_th=0.010),
-        TrendSignalGen("Trend_XRPUSDT_15m", "XRPUSDT", "15m", hysteresis_th=0.002),
+        TrendSignalGen("Trend_XRPUSDT_15m", "XRPUSDT", "15m", hysteresis_th=0.002, confirm_bars=5),
         TrendSignalGen("Trend_XRPUSDT_1h",  "XRPUSDT", "1h",  hysteresis_th=0.005),
         TrendSignalGen("Trend_XRPUSDT_4h",  "XRPUSDT", "4h",  hysteresis_th=0.010),
-        TrendSignalGen("Trend_SOLUSDT_15m", "SOLUSDT", "15m", hysteresis_th=0.002),
+        TrendSignalGen("Trend_SOLUSDT_15m", "SOLUSDT", "15m", hysteresis_th=0.002, confirm_bars=5),
         TrendSignalGen("Trend_SOLUSDT_1h",  "SOLUSDT", "1h",  hysteresis_th=0.005),
         TrendSignalGen("Trend_SOLUSDT_4h",  "SOLUSDT", "4h",  hysteresis_th=0.010),
     )
